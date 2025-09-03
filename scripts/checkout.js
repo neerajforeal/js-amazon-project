e3562469b55e29214e896432362cb2840e351110
neeraj <neerajpallikonda34@gmail.com>
2025-09-03 15:21:57 +0530

    create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
-import { renderOrderSummary } from "./checkout/orderSummary";
-import { renderPaymentSummary } from "./checkout/paymentSummary";
+import { renderOrderSummary } from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import "../data/cart-oop.js";
 
 renderOrderSummary();
 renderPaymentSummary();
