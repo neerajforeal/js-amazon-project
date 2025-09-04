c01d0faf35fc28ba86c6eef614cd8a8334b18142
neeraj <neerajpallikonda34@gmail.com>
2025-09-04 11:00:43 +0530

    Learn promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -72,3 +72,14 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
 
   saveToStorage();
 }
+
+export function loadCart(func) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("load", () => {
+    console.log(xhr.response);
+  });
+
+  xhr.open("GET", "https://supersimplebackend.dev/cart");
+  xhr.send();
+}
